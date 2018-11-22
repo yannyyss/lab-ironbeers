c84d8c0b3f5ad7d471bdfd5e653bbe2824446483
“yannyyss” <yannyyss@hotmail.com>
2018-11-22 16:34:43 -0600

    Boostrap Style Fixed

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,10 +9,31 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.get('/', (req, res, next) => {
+//routes
+
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get("/beers", (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render("beers", {beers: beers}) //You should call the render method after getting the beers
+  })
+  .catch(e => next(e));
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res,next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      let random = beers[Math.floor(Math.random() * beers.length)];
+      res.render('random-beers', {beers: random})
+    })
+    .catch(e => next(e));
+});
+
+app.listen(3000, () => {
+  console.log('listening at 3000');
+});
